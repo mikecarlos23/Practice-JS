5a2de3c90a7ca280e06899a13d28e88e52a69f69
user <user@users-MacBook-Pro.local>
2023-06-23 19:02:06 +0100

    modified Number to list

--- Number-to-list.js
+++ Number-to-list.js
@@ -1,20 +1,4 @@
-// function m(value){
-//     let r=[]
-//     let k=[]
-//        let digit=value.toString()
-//     for(let i=0;i<digit.length;i++){
-//     r.push(digit[i])
-//     }
-//     for(let i=0;i<r.length;i++){
-//         k.push(parseInt(r[i]))
-//     }
-//    let p= k.reduce(function(sum,item){
-// sum=sum+item
-// return sum
-//     },0)
-//     return p
-//     }
-//     console.log(m(253))
+
 function NumberToList(value){
     let r=[]
        let digit=value.toString()
